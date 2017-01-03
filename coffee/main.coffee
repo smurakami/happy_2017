f776bea41c1c94ae8bfe720e54328740be891e8c
s.murakami <smurakami.84@gmail.com>
2017-01-04 05:40:47 +0900

    鷹まで飛ばした

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,8 +1,63 @@
-class Moveup
+class Back
+  constructor: ->
+    @canvas = $('#back').get(0)
+    @context = @canvas.getContext('2d')
+    @width = @canvas.width = $(window).width()
+    @height = @canvas.height = $(window).height()
+
+    @light = new Light(@)
+
+    @objects = []
+    for i in [0...20]
+      obj = new Obj(@)
+
+      @objects.push obj
+
+    @timer = setInterval =>
+      @update()
+      @draw()
+    , 33
+
+  update: ->
+    @light.update()
+    for obj in @objects
+      obj.update()
+
+  draw: ->
+    @context.clearRect(0, 0, @width, @height)
+    # @context.beginPath()
+    # @context.fillRect(0, 0, @width, @height)
+    # @context.fillRect(10, 10, 40, 40)
+    @light.draw()
+    for obj in @objects
+      obj.draw @context
+
+
+  class Light
+    constructor: (back) ->
+      @back = back
+      @context = @back.context
+      @size = Math.sqrt( Math.pow(@back.height, 2) + Math.pow(@back.width, 2)  )
+      @img = new Image()
+      @img.src = 'img/light.jpg'
+      @rotation = 0
+    update: ->
+      @rotation += 0.01
+    draw: ->
+      @context.save()
+      @context.translate(@back.width/2, @back.height/2)
+      @context.rotate(@rotation)
+      @context.translate(-@size/2, -@size/2)
+      @context.drawImage(
+        @img, 0, 0, @img.width, @img.height,
+        0, 0 , @size, @size)
+      @context.restore()
+
+
   class Obj
-    constructor: (parent) ->
-      @parent = parent
-      @context = @parent.context
+    constructor: (back) ->
+      @back = back
+      @context = @back.context
       @size =
         width: 80
         height: 80
@@ -21,10 +76,10 @@ class Moveup
       @pos.x += @vel.x
       @pos.y += @vel.y
       if @pos.y < -@size.height
-        @pos.y = @parent.height
+        @pos.y = @back.height
       if @pos.x < -@size.width
-        @pos.x = @parent.width + @size.width
-      if @pos.x > @parent.width + @size.width
+        @pos.x = @back.width + @size.width
+      if @pos.x > @back.width + @size.width
         @pos.x = -@size.width
       # @vel.y -= 0.1
       @rotation += Math.PI * 0.05
@@ -41,14 +96,15 @@ class Moveup
 
       @context.restore()
 
+class Front
   constructor: ->
-    @canvas = $('#moveup').get(0)
+    @canvas = $('#front').get(0)
     @context = @canvas.getContext('2d')
     @width = @canvas.width = $(window).width()
     @height = @canvas.height = $(window).height()
 
     @objects = []
-    for i in [0...10]
+    for i in [0...5]
       obj = new Obj(@)
 
       @objects.push obj
@@ -71,10 +127,54 @@ class Moveup
       obj.draw @context
 
 
+  class Obj
+    constructor: (back) ->
+      @back = back
+      @context = @back.context
+      @direction = if Math.random() > 0.5 then 1 else -1
+      @size =
+        width: 160
+        height: 160
+      @pos =
+        x: Math.random() * $(window).width()
+        y: (0.5 + Math.random()) * $(window).height()
+      @vel =
+        x: -10 * @direction
+        y: -3
+
+      @img = new Image()
+      @img.src = 'img/taka.png'
+      @rotation = Math.PI * 2 * Math.random()
+
+    update: ->
+      @pos.x += @vel.x
+      @pos.y += @vel.y
+      if @pos.y < -@size.height
+        @pos.y = @back.height
+      if @pos.x < -@size.width
+        @pos.x = @back.width + @size.width
+      if @pos.x > @back.width + @size.width
+        @pos.x = -@size.width
+      # @vel.y -= 0.1
+      @rotation += Math.PI * 0.05
+    draw: ->
+      @context.save()
+      @context.translate(@pos.x, @pos.y)
+      @context.scale(@direction, 1)
+      @context.translate(-@size.width/2, -@size.height/2)
+
+
+      @context.drawImage(
+        @img, 0, 0, @img.width, @img.height,
+        0, 0 , @size.width, @size.height)
+
+      @context.restore()
+
 
 class Main
   constructor: ->
-    new Moveup()
+    new Back()
+    new Front()
     $('body').css 'height', $(window).height()
     @initLight()
     # @initObjects()
--- js/main.js
+++ js/main.js
@@ -1,13 +1,85 @@
 (function() {
-  var Main, Moveup;
+  var Back, Front, Main;
 
-  Moveup = (function() {
-    var Obj;
+  Back = (function() {
+    var Light, Obj;
+
+    function Back() {
+      var i, obj, _i;
+      this.canvas = $('#back').get(0);
+      this.context = this.canvas.getContext('2d');
+      this.width = this.canvas.width = $(window).width();
+      this.height = this.canvas.height = $(window).height();
+      this.light = new Light(this);
+      this.objects = [];
+      for (i = _i = 0; _i < 20; i = ++_i) {
+        obj = new Obj(this);
+        this.objects.push(obj);
+      }
+      this.timer = setInterval((function(_this) {
+        return function() {
+          _this.update();
+          return _this.draw();
+        };
+      })(this), 33);
+    }
+
+    Back.prototype.update = function() {
+      var obj, _i, _len, _ref, _results;
+      this.light.update();
+      _ref = this.objects;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        obj = _ref[_i];
+        _results.push(obj.update());
+      }
+      return _results;
+    };
+
+    Back.prototype.draw = function() {
+      var obj, _i, _len, _ref, _results;
+      this.context.clearRect(0, 0, this.width, this.height);
+      this.light.draw();
+      _ref = this.objects;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        obj = _ref[_i];
+        _results.push(obj.draw(this.context));
+      }
+      return _results;
+    };
+
+    Light = (function() {
+      function Light(back) {
+        this.back = back;
+        this.context = this.back.context;
+        this.size = Math.sqrt(Math.pow(this.back.height, 2) + Math.pow(this.back.width, 2));
+        this.img = new Image();
+        this.img.src = 'img/light.jpg';
+        this.rotation = 0;
+      }
+
+      Light.prototype.update = function() {
+        return this.rotation += 0.01;
+      };
+
+      Light.prototype.draw = function() {
+        this.context.save();
+        this.context.translate(this.back.width / 2, this.back.height / 2);
+        this.context.rotate(this.rotation);
+        this.context.translate(-this.size / 2, -this.size / 2);
+        this.context.drawImage(this.img, 0, 0, this.img.width, this.img.height, 0, 0, this.size, this.size);
+        return this.context.restore();
+      };
+
+      return Light;
+
+    })();
 
     Obj = (function() {
-      function Obj(parent) {
-        this.parent = parent;
-        this.context = this.parent.context;
+      function Obj(back) {
+        this.back = back;
+        this.context = this.back.context;
         this.size = {
           width: 80,
           height: 80
@@ -29,12 +101,12 @@
         this.pos.x += this.vel.x;
         this.pos.y += this.vel.y;
         if (this.pos.y < -this.size.height) {
-          this.pos.y = this.parent.height;
+          this.pos.y = this.back.height;
         }
         if (this.pos.x < -this.size.width) {
-          this.pos.x = this.parent.width + this.size.width;
+          this.pos.x = this.back.width + this.size.width;
         }
-        if (this.pos.x > this.parent.width + this.size.width) {
+        if (this.pos.x > this.back.width + this.size.width) {
           this.pos.x = -this.size.width;
         }
         return this.rotation += Math.PI * 0.05;
@@ -53,14 +125,21 @@
 
     })();
 
-    function Moveup() {
+    return Back;
+
+  })();
+
+  Front = (function() {
+    var Obj;
+
+    function Front() {
       var i, obj, _i;
-      this.canvas = $('#moveup').get(0);
+      this.canvas = $('#front').get(0);
       this.context = this.canvas.getContext('2d');
       this.width = this.canvas.width = $(window).width();
       this.height = this.canvas.height = $(window).height();
       this.objects = [];
-      for (i = _i = 0; _i < 10; i = ++_i) {
+      for (i = _i = 0; _i < 5; i = ++_i) {
         obj = new Obj(this);
         this.objects.push(obj);
       }
@@ -72,7 +151,7 @@
       })(this), 33);
     }
 
-    Moveup.prototype.update = function() {
+    Front.prototype.update = function() {
       var obj, _i, _len, _ref, _results;
       _ref = this.objects;
       _results = [];
@@ -83,7 +162,7 @@
       return _results;
     };
 
-    Moveup.prototype.draw = function() {
+    Front.prototype.draw = function() {
       var obj, _i, _len, _ref, _results;
       this.context.clearRect(0, 0, this.width, this.height);
       _ref = this.objects;
@@ -95,13 +174,64 @@
       return _results;
     };
 
-    return Moveup;
+    Obj = (function() {
+      function Obj(back) {
+        this.back = back;
+        this.context = this.back.context;
+        this.direction = Math.random() > 0.5 ? 1 : -1;
+        this.size = {
+          width: 160,
+          height: 160
+        };
+        this.pos = {
+          x: Math.random() * $(window).width(),
+          y: (0.5 + Math.random()) * $(window).height()
+        };
+        this.vel = {
+          x: -10 * this.direction,
+          y: -3
+        };
+        this.img = new Image();
+        this.img.src = 'img/taka.png';
+        this.rotation = Math.PI * 2 * Math.random();
+      }
+
+      Obj.prototype.update = function() {
+        this.pos.x += this.vel.x;
+        this.pos.y += this.vel.y;
+        if (this.pos.y < -this.size.height) {
+          this.pos.y = this.back.height;
+        }
+        if (this.pos.x < -this.size.width) {
+          this.pos.x = this.back.width + this.size.width;
+        }
+        if (this.pos.x > this.back.width + this.size.width) {
+          this.pos.x = -this.size.width;
+        }
+        return this.rotation += Math.PI * 0.05;
+      };
+
+      Obj.prototype.draw = function() {
+        this.context.save();
+        this.context.translate(this.pos.x, this.pos.y);
+        this.context.scale(this.direction, 1);
+        this.context.translate(-this.size.width / 2, -this.size.height / 2);
+        this.context.drawImage(this.img, 0, 0, this.img.width, this.img.height, 0, 0, this.size.width, this.size.height);
+        return this.context.restore();
+      };
+
+      return Obj;
+
+    })();
+
+    return Front;
 
   })();
 
   Main = (function() {
     function Main() {
-      new Moveup();
+      new Back();
+      new Front();
       $('body').css('height', $(window).height());
       this.initLight();
       this.counter = 0;
