b6c868970dc9b128c59eec7a12e21a0d891ba6ff
s.murakami <smurakami.84@gmail.com>
2017-01-04 05:07:27 +0900

    ナスまで飛ばせた

--- coffee/main.coffee
+++ coffee/main.coffee
@@ -1,4 +1,127 @@
-$ ->
-  console.log 'hello'
-  $('body').css 'height', $('window').height()
+class Moveup
+  class Obj
+    constructor: (parent) ->
+      @parent = parent
+      @context = @parent.context
+      @size =
+        width: 80
+        height: 80
+      @pos =
+        x: Math.random() * $(window).width()
+        y: (0.5 + Math.random()) * $(window).height()
+      @vel =
+        x: (Math.random() - 0.5) * 2
+        y: -3
 
+      @img = new Image()
+      @img.src = 'img/nasu.png'
+      @rotation = Math.PI * 2 * Math.random()
+
+    update: ->
+      @pos.x += @vel.x
+      @pos.y += @vel.y
+      if @pos.y < -@size.height
+        @pos.y = @parent.height
+      if @pos.x < -@size.width
+        @pos.x = @parent.width + @size.width
+      if @pos.x > @parent.width + @size.width
+        @pos.x = -@size.width
+      # @vel.y -= 0.1
+      @rotation += Math.PI * 0.05
+    draw: ->
+      @context.save()
+      @context.translate(@pos.x, @pos.y)
+      @context.rotate(@rotation)
+      @context.translate(-@size.width/2, -@size.height/2)
+
+
+      @context.drawImage(
+        @img, 0, 0, @img.width, @img.height,
+        0, 0 , @size.width, @size.height)
+
+      @context.restore()
+
+  constructor: ->
+    @canvas = $('#moveup').get(0)
+    @context = @canvas.getContext('2d')
+    @width = @canvas.width = $(window).width()
+    @height = @canvas.height = $(window).height()
+
+    @objects = []
+    for i in [0...10]
+      obj = new Obj(@)
+
+      @objects.push obj
+
+    @timer = setInterval =>
+      @update()
+      @draw()
+    , 33
+
+  update: ->
+    for obj in @objects
+      obj.update()
+
+  draw: ->
+    @context.clearRect(0, 0, @width, @height)
+    # @context.beginPath()
+    # @context.fillRect(0, 0, @width, @height)
+    # @context.fillRect(10, 10, 40, 40)
+    for obj in @objects
+      obj.draw @context
+
+
+
+class Main
+  constructor: ->
+    new Moveup()
+    $('body').css 'height', $(window).height()
+    @initLight()
+    # @initObjects()
+    @counter = 0
+    @timer = setInterval =>
+      @update()
+    , 33
+
+  initLight: ->
+    height = $(window).height()
+    width = $(window).width()
+    size = Math.sqrt(height * height + width * width)
+    $('#light').css 'width', size
+    $('#light').css 'height', size
+    $('#light').css 'margin-left', (width - size)/2
+    $('#light').css 'margin-top', (height - size)/2
+
+  initObjects: ->
+    @objects = []
+    for i in [0...10]
+      obj = $('.object').clone()
+      obj.pos =
+        x: Math.random() * $(window).width()
+        # y: (1 + Math.random()) * $(window).height()
+        y: 10
+
+      obj.vel =
+        x: 0
+        y: -1
+
+      obj.css 'margin-left', obj.pos.x
+      obj.css 'margin-top', obj.pos.y
+      $('#objects').append obj
+      @objects.push obj
+
+  update: ->
+    # $('#light').css 'transform', "rotate(#{@counter}deg)"
+
+    # for obj in @objects
+    #   obj.pos.x += obj.vel.x
+    #   obj.pos.y += obj.vel.y
+
+    #   obj.css 'margin-left', obj.pos.x
+    #   obj.css 'margin-top', obj.pos.y
+
+    @counter++
+
+
+$ ->
+  new Main
--- js/main.js
+++ js/main.js
@@ -1,7 +1,160 @@
 (function() {
+  var Main, Moveup;
+
+  Moveup = (function() {
+    var Obj;
+
+    Obj = (function() {
+      function Obj(parent) {
+        this.parent = parent;
+        this.context = this.parent.context;
+        this.size = {
+          width: 80,
+          height: 80
+        };
+        this.pos = {
+          x: Math.random() * $(window).width(),
+          y: (0.5 + Math.random()) * $(window).height()
+        };
+        this.vel = {
+          x: (Math.random() - 0.5) * 2,
+          y: -3
+        };
+        this.img = new Image();
+        this.img.src = 'img/nasu.png';
+        this.rotation = Math.PI * 2 * Math.random();
+      }
+
+      Obj.prototype.update = function() {
+        this.pos.x += this.vel.x;
+        this.pos.y += this.vel.y;
+        if (this.pos.y < -this.size.height) {
+          this.pos.y = this.parent.height;
+        }
+        if (this.pos.x < -this.size.width) {
+          this.pos.x = this.parent.width + this.size.width;
+        }
+        if (this.pos.x > this.parent.width + this.size.width) {
+          this.pos.x = -this.size.width;
+        }
+        return this.rotation += Math.PI * 0.05;
+      };
+
+      Obj.prototype.draw = function() {
+        this.context.save();
+        this.context.translate(this.pos.x, this.pos.y);
+        this.context.rotate(this.rotation);
+        this.context.translate(-this.size.width / 2, -this.size.height / 2);
+        this.context.drawImage(this.img, 0, 0, this.img.width, this.img.height, 0, 0, this.size.width, this.size.height);
+        return this.context.restore();
+      };
+
+      return Obj;
+
+    })();
+
+    function Moveup() {
+      var i, obj, _i;
+      this.canvas = $('#moveup').get(0);
+      this.context = this.canvas.getContext('2d');
+      this.width = this.canvas.width = $(window).width();
+      this.height = this.canvas.height = $(window).height();
+      this.objects = [];
+      for (i = _i = 0; _i < 10; i = ++_i) {
+        obj = new Obj(this);
+        this.objects.push(obj);
+      }
+      this.timer = setInterval((function(_this) {
+        return function() {
+          _this.update();
+          return _this.draw();
+        };
+      })(this), 33);
+    }
+
+    Moveup.prototype.update = function() {
+      var obj, _i, _len, _ref, _results;
+      _ref = this.objects;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        obj = _ref[_i];
+        _results.push(obj.update());
+      }
+      return _results;
+    };
+
+    Moveup.prototype.draw = function() {
+      var obj, _i, _len, _ref, _results;
+      this.context.clearRect(0, 0, this.width, this.height);
+      _ref = this.objects;
+      _results = [];
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        obj = _ref[_i];
+        _results.push(obj.draw(this.context));
+      }
+      return _results;
+    };
+
+    return Moveup;
+
+  })();
+
+  Main = (function() {
+    function Main() {
+      new Moveup();
+      $('body').css('height', $(window).height());
+      this.initLight();
+      this.counter = 0;
+      this.timer = setInterval((function(_this) {
+        return function() {
+          return _this.update();
+        };
+      })(this), 33);
+    }
+
+    Main.prototype.initLight = function() {
+      var height, size, width;
+      height = $(window).height();
+      width = $(window).width();
+      size = Math.sqrt(height * height + width * width);
+      $('#light').css('width', size);
+      $('#light').css('height', size);
+      $('#light').css('margin-left', (width - size) / 2);
+      return $('#light').css('margin-top', (height - size) / 2);
+    };
+
+    Main.prototype.initObjects = function() {
+      var i, obj, _i, _results;
+      this.objects = [];
+      _results = [];
+      for (i = _i = 0; _i < 10; i = ++_i) {
+        obj = $('.object').clone();
+        obj.pos = {
+          x: Math.random() * $(window).width(),
+          y: 10
+        };
+        obj.vel = {
+          x: 0,
+          y: -1
+        };
+        obj.css('margin-left', obj.pos.x);
+        obj.css('margin-top', obj.pos.y);
+        $('#objects').append(obj);
+        _results.push(this.objects.push(obj));
+      }
+      return _results;
+    };
+
+    Main.prototype.update = function() {
+      return this.counter++;
+    };
+
+    return Main;
+
+  })();
+
   $(function() {
-    console.log('hello');
-    return $('body').css('height', $('window').height());
+    return new Main;
   });
 
 }).call(this);
